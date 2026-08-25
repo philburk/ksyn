9696454b029b20426b415824984b5d9cb9edf23e
Phil Burk <philburk@mobileer.com>
2026-08-25 10:48:54 -0700

    delete unused Oscillator test file

--- ksyn/src/commonMain/kotlin/com/softsynth/ksyn/Oscillator.kt
+++
@@ -1,9 +0,0 @@
-package com.softsynth.ksyn
-import kotlin.math.sin
-import kotlin.math.PI
-
-class Oscillator {
-    fun getSine(phase: Double): Double {
-        return sin(phase * 2 * PI)
-    }
-}
